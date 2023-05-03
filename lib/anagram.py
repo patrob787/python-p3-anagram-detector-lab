9e2b479767c44dce46cd9ca577d3cf1e630487f7
Patrick Roberts <patrob22@gmail.com>
2023-05-03 12:52:58 -0400

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
-# your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        def split(word):
+          return [letter for letter in word]
+      
+        split_word = split(self.word)
+        split_word.sort()
+
+        matches = []
+        
+        for item in list:
+          split_item = split(item)
+          split_item.sort()
+          
+          if split_word == split_item:
+            matches.append(item)
+        return matches
